22dd8ee6383d9042555a3c0c71a660427b3c3004
3drise <78169614+3drise@users.noreply.github.com>
2023-01-27 10:53:42 +0100

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+namespace testled {
+    /**
+     * test led sur port P0
+     */
+
+ pins.digitalWritePin(DigitalPin.P0, 1),
+    basic.pause(800),
+     pins.digitalWritePin(DigitalPin.P0, 1)
+
+}
