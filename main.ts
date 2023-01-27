b62c85a828df514371f09b417d32774bda30cf93
3drise <78169614+3drise@users.noreply.github.com>
2023-01-27 10:55:09 +0100

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,10 @@ namespace testled {
     /**
      * test led sur port P0
      */
-
+//% blockId=ledsurP0
+//% block="lanceLED"
  pins.digitalWritePin(DigitalPin.P0, 1),
-    basic.pause(800),
+    basic.pause(8000),
      pins.digitalWritePin(DigitalPin.P0, 1)
 
 }
