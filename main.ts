dedfab5de41e1b59f9b917bca1bee03e82a73f12
3drise <78169614+3drise@users.noreply.github.com>
2023-01-27 11:00:17 +0100

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,10 @@ namespace testled {
 //% blockId=ledsurP0
 //% block="lanceLED"
 //% index.min=0 index.max=25
- pins.digitalWritePin(DigitalPin.P0, 1),
-    basic.pause(8000),
-     pins.digitalWritePin(DigitalPin.P0, 1),
-    basic.pause(8000)
+ export function plotAt(index: number): void {
+     index |=0
+     const y = Math.floor(index / 5);
+     const x = Math.floor(index % 5)
+     led.plot(x, y)
+ }
 }
